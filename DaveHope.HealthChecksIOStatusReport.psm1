3188d76be2317c7a87e57ce41ab6b8475d54d2ee
Dave Hope <dave@hope.mx>
2023-08-22 08:39:05 +0000

    Enforce TLS 1.2

--- DaveHope.HealthChecksIOStatusReport.psm1
+++ DaveHope.HealthChecksIOStatusReport.psm1
@@ -10,23 +10,30 @@
         [Parameter(ParameterSetName='Success')][Switch]$Success,
         [Parameter(ParameterSetName='Failure')][Switch]$Failure,
         [Parameter(ParameterSetName='Start')][Switch]$Start,
-        [Parameter(Mandatory=$true)][string]$uuid,
+        [Parameter(Mandatory)][string]$uuid,
         [Parameter()][string]$AttachLogs,
 	    [Parameter()][Int32]$TimeoutSec = 10
         )
 
-    $uri = "https://hc-ping.com/$uuid"
+    $uri = 'https://hc-ping.com/' + $uuid
 
     if($Success){}
-    If($Failure){ $uri += "/fail" }
-    If($Start){ $uri += "/start"}
+    If($Failure){ $uri += '/fail' }
+    If($Start){ $uri += '/start' }
 
     $irmArguments = @{
-	Uri = $uri
-	TimeoutSec = $TimeoutSec
+	    Uri        = $uri
+	    TimeoutSec = $TimeoutSec
     }
 
-    $httpRes = $null
+    # WindowsPowerShell 5.1 will likely be set to 'Ssl3, Tls' (SSL3, TLS1.0) on
+    # platforms such as Windows Server 2016. Add Tls12 in preparation for HCIO
+    # disabling legacy TLS protocols.
+    if ([Net.ServicePointManager]::SecurityProtocol -notcontains [Net.SecurityProtocolType]::Tls12)
+    {
+        [Net.ServicePointManager]::SecurityProtocol += [Net.SecurityProtocolType]::Tls12
+    }
+
     try
     {
         if($null -ne $AttachLogs)
@@ -36,17 +43,15 @@
             # request body. If the request body looks like a UTF-8 string,
             # Healthchecks.io will log the first 10 kilobytes of the request
             # body, so you can inspect it later.
-            $httpRes = Invoke-RestMethod -Method Post -Body $AttachLogs @irmArguments
-            Write-Debug "Received $($httpRes.StatusCode) from $uri"
+            Invoke-RestMethod -Method Post -Body $AttachLogs @irmArguments | Out-Null
         }
         else
         {
-            $httpRes = Invoke-RestMethod @irmArguments
+            Invoke-RestMethod @irmArguments | Out-Null
         }
     }
     catch
     {
-        Write-Warning "Failed to report status - $_"
+        Write-Warning ('Failed to report status - {0}' -f $_)
     }
-
 }
